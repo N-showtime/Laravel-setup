52c25d09f32d4f1cb27ef18eb4ae03b73283c564
Shogo.N <hozukl.1225@gmail.com>
2025-10-04 13:43:13 +0900

    test

--- myproject/routes/web.php
+++ myproject/routes/web.php
@@ -6,6 +6,10 @@ use App\Livewire\Settings\Profile;
 use App\Livewire\Settings\TwoFactor;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
+use App\Http\Controllers\TestController;
+
+Route::get('/test', [TestController::class, 'test'])
+->name('test');
 
 Route::get('/', function () {
     return view('welcome');
